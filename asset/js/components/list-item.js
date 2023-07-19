736c999f193ede890a881159799979af4cd078aa
Ceylin <caltepeceylin@gmail.com>
2023-07-20 00:01:43 +0300

    feat: create upload, create, delete and filter functions

--- asset/js/components/list-item.js
+++ asset/js/components/list-item.js
@@ -1,15 +1,18 @@
 let menuButtons;
-
+let checkboxs;
 
 function findListItem() {
     menuButtons = document.querySelectorAll(".todo-menu-button");
-    addEvent();
+    checkboxs = document.querySelectorAll("input[type='checkbox']");
 }
 
 function addEvent() {
     menuButtons.forEach((menuButton) => menuButton.addEventListener("click", () => {
         menuButton.nextElementSibling.classList.toggle("open");
     }, true));
+    checkboxs.forEach((checkbox) => checkbox.addEventListener("change", function(){
+        completeTodo(checkbox.parentElement.id, checkbox.checked);
+    }));
 }
 
 window.onclick = function (event) {
@@ -19,19 +22,19 @@ window.onclick = function (event) {
         }
         else if (menuButton.nextElementSibling.contains(event.target)) {
             if (event.target.classList.contains("cancel-button")) {
+                deleteToDo(menuButton.parentElement.id);
                 menuButton.parentElement.remove();
             }
             else if (event.target.classList.contains("update-button")) {
                 let todoText = menuButton.previousElementSibling.previousElementSibling.textContent;
-                menuButton.previousElementSibling.previousElementSibling.innerHTML = '<input type="text"><div class="save-button">Save</div>';
-                menuButton.previousElementSibling.previousElementSibling.firstChild.value = todoText;
+                menuButton.previousElementSibling.previousElementSibling.innerHTML = '<input type="text"><div class="save-button">Save</div>'; //.todo-text
+                menuButton.previousElementSibling.previousElementSibling.firstChild.value = todoText; //.todo-text input
                 menuButton.previousElementSibling.previousElementSibling.lastChild.addEventListener("click", function(){
-                    todoText = menuButton.previousElementSibling.previousElementSibling.firstChild.value;
-                    menuButton.previousElementSibling.previousElementSibling.innerHTML = todoText;
+                    todoText = menuButton.previousElementSibling.previousElementSibling.firstChild.value; //.todo-text input
+                    menuButton.previousElementSibling.previousElementSibling.innerHTML = todoText; //.todo-text
+                    updateTodo(menuButton.parentElement.id, todoText);
                 });
             }
         }
     })
 }
-
-findListItem();
